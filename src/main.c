e5a469802f0db192b92b5c7e10a7ccf055f874e9
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-06-24 10:47:09 +0100

    without table

--- src/main.c
+++ src/main.c
@@ -2,60 +2,54 @@
 #include <math.h>
 #include <stdio.h>
 
-#define MAX 130
+#define MAX 18
 
 double fct(double x) { return (sqrt(x)); }
 
-double qu_trapezes_adapt(double a, double b, double eps, int n);
-double trapezoidal(double a, double b, double eps, int n);
+double trapezes_adapt(double a, double b, double eps, int n);
 
 int main() {
-  int n = 10000;
-  double a, b, eps, solution;
+  double a, b, n, eps, solution;
+  n = 100000;
   a = 1;
   b = 1.3;
-  eps = 1e-15;
-  // solution = qu_trapezes_adapt(a, b, eps, n);
-  solution = trapezoidal(a, b, eps, n);
+  eps = 0.00000000001;
+
+  solution = trapezes_adapt(a, b, eps, n);
+
   printf("Méthode des trapèzes (formule adaptative)\n");
-  printf("I = %.20e", solution);
+  printf("I = %.20lf", solution);
 
   return 0;
 }
 
-double qu_trapezes_adapt(double a, double b, double eps, int n) {
-  long i, j, p;
-  double h, s, err, t0, t;
-  j = 1;
-  p = 1;
+double trapezes_adapt(double a, double b, double eps, int n) {
+  int i, j;
+  double h, sum, err, x, intergral;
+
+  n = 1;
   h = b - a;
-  t0 = h * (fct(a) + fct(b)) / 2;
+  j = 0;
   err = 2 * eps;
-  while ((fabs(err) > eps) && (j <= 130)) {
-    h = h / 2.0;
-    s = 0;
-    for (i = 1; i <= (2 * p - 1); i++) {
-      s += fct(a + (2 * i - 1) * h);
-    }
-    t = t0 / 2.0 + h * s;
-    err = t - t0;
-    t0 = t;
-    p++;
+
+  intergral = h * (fct(a) + fct(b)) / 2;
+
+  while (fabs(err) > eps && j < MAX - 1) {
     j++;
+    err = intergral;
+
+    n = n * 2;
+    h = h / 2;
+    x = a - h;
+
+    sum = 0;
+    for (i = 1; i <= n / 2; i++) {
+      x = x + 2 * h;
+      sum = sum + fct(x);
+    }
+    intergral = intergral / 2 + h * sum;
+    err = err - intergral;
   }
-  return t;
-}
 
-double trapezoidal(double a, double b, double eps, int n) {
-  n = MAX - 1;
-  double h = (b - a) / n;
-  double integral = 0.0;
-  // Vector x(n+1,0.0);
-  double x[MAX];
-  for (unsigned int p = 0; p <= n; p++)
-    x[p] = a + p * h;
-  for (unsigned int k = 1; k <= n; k++)
-    integral += (fct(x[k - 1]) + fct(x[k])) * (h / 2);
-
-  return integral;
+  return intergral;
 }
